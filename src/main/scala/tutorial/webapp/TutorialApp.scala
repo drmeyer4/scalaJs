a4574b891814bb8315a4734ac4381b15e3513d60
David Meyer <david.meyer@Davids-MacBook-Pro.local>
2017-09-29 09:54:59 -0400

    continueing tutorial

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,22 +2,19 @@ package tutorial.webapp
 
 import org.scalajs.dom
 import dom.document
-import scala.scalajs.js.annotation.JSExportTopLevel
+import org.scalajs.jquery.jQuery
 
 object TutorialApp {
 	def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Hello World")
+    jQuery(() => setupUI())
 	}
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(() => addClickedMessage())
+    jQuery("body").append("<p>Hello World</p>")
   }
 
-  @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "button clicked")
+    jQuery("body").append("<p>has been clicked</p>")
   }
 }
